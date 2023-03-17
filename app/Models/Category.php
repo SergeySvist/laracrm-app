f017bb78e29d5952baf6bedbb1689aaba03421dd
SergeySvist <sergeysvist557@gmail.com>
2023-03-17 14:07:57 +0200

    add: ide-helper and annotations to models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use Database\Factories\CategoryFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Category
+ *
+ * @property int $id
+ * @property string $title
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, SubCategory> $subCategories
+ * @property-read int|null $sub_categories_count
+ * @method static CategoryFactory factory($count = null, $state = [])
+ * @method static Builder|Category newModelQuery()
+ * @method static Builder|Category newQuery()
+ * @method static Builder|Category query()
+ * @method static Builder|Category whereCreatedAt($value)
+ * @method static Builder|Category whereId($value)
+ * @method static Builder|Category whereTitle($value)
+ * @method static Builder|Category whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Category extends Model
 {
     use HasFactory;
