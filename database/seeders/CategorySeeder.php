75f3302b7cd670f3726b3e749a02bbab43a7b40c
SergeySvist <sergeysvist557@gmail.com>
2023-03-17 15:22:58 +0200

    work with seeders and factories

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\SubCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::factory(10)
+            ->has(SubCategory::factory()->count(rand(5,10)), 'subCategories')
+            ->create();
+    }
+}
