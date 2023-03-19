6f1dc08d874a7cb911c310eee70effb28f68c84d
SergeySvist <sergeysvist557@gmail.com>
2023-03-19 15:57:33 +0200

    add: routes and controller for Categoryes

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponser;
+
+class CategoryController extends Controller
+{
+    use ApiResponser;
+    public function index(){
+        return $this->successResponse(Category::all()->toArray());
+    }
+
+    public function get(Category $category){
+        return $this->successResponse($category->toArray());
+
+    }
+
+    public function update(Category $category){
+
+    }
+
+    public function delete(Category $category){
+
+    }
+
+    public function create(Request $request){
+
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => 'categories'], function (){
+    Route::get('/{category}', [CategoryController::class, 'get']);
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::post('/', [CategoryController::class, 'create']);
+    Route::patch('/{category}', [CategoryController::class, 'update']);
+    Route::delete('/{category}', [CategoryController::class, 'delete']);
+});
